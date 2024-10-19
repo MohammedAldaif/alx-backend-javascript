34608662f6a118f9388aa9155af115bfcf666b0b
MohammedAldaif <mohammedaldaif71@gmail.com>
2024-10-19 11:57:47 -0700

    M

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,14 +1,14 @@
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
-    Promise.all([uploadPhoto(), createUser()])
-        .then(([photo, user]) => {
-            console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
-        })
-        .catch(() => {
-            console.log("Signup system offline");
-        });
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      const { body } = values[0];
+      const { firstName } = values[1];
+      const { lastName } = values[1];
+      console.log(`${body} ${firstName} ${lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
 
 export default handleProfileSignup;
-
