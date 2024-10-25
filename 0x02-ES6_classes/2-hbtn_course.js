43075c3898aeee1ab9b5dc48078cbdaf8cba1685
MohammedAldaif <mohammedaldaif71@gmail.com>
2024-10-25 11:53:25 -0700

    M

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,34 +1,26 @@
+// 2-hbtn_course.js
 class HolbertonCourse {
-  // Constructor
   constructor(name, length, students) {
-    // Validate the types
     this._validateString(name, 'Name');
     this._validateNumber(length, 'Length');
     this._validateArrayOfStrings(students, 'Students');
 
-    // Store values in "underscore" versions
     this._name = name;
     this._length = length;
     this._students = students;
   }
 
-  // Getters
   get name() {
     return this._name;
   }
 
-  get length() {
-    return this._length;
-  }
-
-  get students() {
-    return this._students;
-  }
-
-  // Setters with validation
   set name(newName) {
     this._validateString(newName, 'Name');
     this._name = newName;
+  }
+
+  get length() {
+    return this._length;
   }
 
   set length(newLength) {
@@ -36,26 +28,27 @@ class HolbertonCourse {
     this._length = newLength;
   }
 
+  get students() {
+    return this._students;
+  }
+
   set students(newStudents) {
     this._validateArrayOfStrings(newStudents, 'Students');
     this._students = newStudents;
   }
 
-  // Helper method to validate string type
   _validateString(value, attribute) {
     if (typeof value !== 'string') {
       throw new TypeError(`${attribute} must be a string`);
     }
   }
 
-  // Helper method to validate number type
   _validateNumber(value, attribute) {
     if (typeof value !== 'number') {
       throw new TypeError(`${attribute} must be a number`);
     }
   }
 
-  // Helper method to validate array of strings
   _validateArrayOfStrings(value, attribute) {
     if (!Array.isArray(value) || !value.every((el) => typeof el === 'string')) {
       throw new TypeError(`${attribute} must be an array of strings`);
